3b974dd4fa092522200c26a9ace6a0e8142fee02
ACM UCR <fake@domain.com>
2024-06-25 01:28:26 +0000

    convert images to webp

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import logo from "@/public/logo.png";
+import logo from "@/public/logo.webp";
 import { items } from "@/data/Navigation";
 
 const NavBar = () => {
@@ -51,7 +51,7 @@ export default NavBar;
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import logo from '@/public/logo.png'; 
+import logo from '@/public/logo.webp'; 
 
 const NavBar = () => {
   return (
