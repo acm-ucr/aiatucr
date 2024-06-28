1a00e49b1f87ed7c82b1102e1044dae4b3567116
Aryan Goel <52222311+agoel27@users.noreply.github.com>
2024-06-28 21:57:15 +0000

    Removed all comments

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,7 @@
-// Landing page
-// This page includes: navbar, introduction, information, offerings, and footer components
-
-// Import the Introduction component
 import Introduction from "@/components/landing/Introduction";
 
 const Home = () => {
-  return (
-    // Displays the Introduction component on the landing page
-    <Introduction />
-  );
+  return <Introduction />;
 };
 
 export default Home;
