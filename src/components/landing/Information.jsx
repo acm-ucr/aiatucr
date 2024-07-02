60d9be4a2cdc29640d348b86763721fef29a1223
khushi-04 <khushi.chaudhari@gmail.com>
2024-07-02 15:13:08 -0700

    added responsive design

--- src/components/landing/Information.jsx
+++ src/components/landing/Information.jsx
@@ -1,18 +1,18 @@
 import Image from "next/image";
-import image from "/public/transparent_logo_1.webp";
+import aiAtUcrLogo from "@/public/transparent_logo_1.webp";
 
 const Information = () => {
   return (
-    <div className="flex items-center">
-      <div className="w-1/2 ml-4">
+    <div className="flex flex-col md:flex-row items-center">
+      <div className="w-full md:w-1/2 md:ml-4 mb-4 md:mb-0">
         <Image
           className="object-contain w-full h-auto"
-          src={image}
-          alt="Logo 1"
+          src={aiAtUcrLogo}
+          alt="AI@UCR Logo"
         />
       </div>
-      <div className="w-7/12 px-4">
-        <p className="text-4xl leading-tight text-center my-8">
+      <div className="w-full md:w-7/12 px-4">
+        <p className="text-xl md:text-4xl leading-tight text-center my-8">
           AI at UCR is the hub for all things Artificial Intelligence. We are
           committed to introducing AI and ML enthusiasts to the wider horizons
           of this evolving field.
