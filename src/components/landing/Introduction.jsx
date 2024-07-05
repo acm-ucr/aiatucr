922a97f53ced3d251f9bb1588061903b394ecda1
ACM UCR <fake@domain.com>
2024-07-05 01:23:59 +0000

    convert images to webp

--- src/components/landing/Introduction.jsx
+++ src/components/landing/Introduction.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import circuitImage from "@/public/circuit.png";
+import circuitImage from "@/public/circuit.webp";
 import { TiArrowSortedDown } from "react-icons/ti";
 
 const Introduction = () => {
