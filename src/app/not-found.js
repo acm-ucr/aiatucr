8767aca441081366000dc95a0be281041697aa82
Zachary Nobunaga <znobunaga11@gmail.com>
2024-07-07 19:54:57 -0700

    changed to ai colors and updated whitespace

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -3,17 +3,15 @@ import Link from "next/link";
 const NotFound = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <p className=" leading-none font-mono text-9xl text-sky-500"> 404 </p>
-      <p className="text-zinc-950 text-3xl font-mono pb-12">
-        {" "}
-        page not found!{" "}
+      <p className=" leading-none font-mono text-9xl text-ai-blue-500 pb-2">
+        404
       </p>
-      <Link href="/">
-        {" "}
-        <a className="text-lg px-8 py-2 rounded-full bg-sky-500 hover:bg-sky-700 text-white">
-          {" "}
-          home{" "}
-        </a>
+      <p className="text-ai-black text-3xl font-mono pb-10"> page not found!</p>
+      <Link
+        href="/"
+        className="text-lg px-9 py-1 rounded-full bg-ai-blue-400 hover:bg-ai-blue-500 text-white"
+      >
+        home
       </Link>
     </div>
   );
