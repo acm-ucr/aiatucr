ebfa160bbe5b61dfdb8197f13adc240238d34dbd
Aryan Goel <52222311+agoel27@users.noreply.github.com>
2024-07-11 20:28:03 +0000

    Used padding for button size and removed changes from tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -10,29 +7,17 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    fontSize: {
-      "3xs": "0.375rem",
-      "2xs": "0.625rem",
-      ...defaultTheme.fontSize,
-    },
-    screens: {
-      xs: "475px",
-      ...defaultTheme.screens,
-    },
     extend: {
       colors: {
         ai: {
-          "white-100": "#FFFFFF",
-          "gray-100": "#98989826",
-          "blue-100": "#4F9AD7",
-          "blue-200": "#77ABD4",
+          "blue-600": "#47759B",
+          "blue-500": "#4F9AD7",
+          "blue-400": "#77ABD4",
           "blue-300": "#9EBED7",
-          "blue-400": "#AEC6D8",
-          "black-100": "#2E2E2E",
+          "blue-200": "#AEC6D8",
+          black: "#2E2E2E",
+          gray: "#323232",
         },
-      },
-      height: {
-        "1/20": "5%",
       },
     },
   },
