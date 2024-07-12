8bcc341d699aa1961bc4346a976d34ad33e28949
Zachary Nobunaga <znobunaga11@gmail.com>
2024-07-12 08:23:59 -0700

    added 500 internal server error page

--- src/app/error.js
+++ src/app/error.js
@@ -1,7 +1,25 @@
 "use client";
 
+import React from 'react';
+import Link from "next/link";
+
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <>
+    <div className="flex flex-col items-center justify-center min-h-screen">  
+      <p className="text-6xl font-bold text-red-600 mb-4">500 -Internal Server Error</p>
+      <p className="text-lg text-gray-700 mb-8">Something went wrong on our end. Please try refreshing the page, or go back to the </p>
+      <Link
+        href="/"
+        className="text-lg px-9 py-1 rounded-full bg-ai-blue-400 hover:bg-ai-blue-500 text-white"
+      >
+        home
+      </Link>
+
+
+    </div>
+    </>
+  );
 };
 
 export default Error;
