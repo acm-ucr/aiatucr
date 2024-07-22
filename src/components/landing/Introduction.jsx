dff7edbe61a84dd55f52f7d5d2f93c9e939a9791
Andrew Khadder <andrewkh15@gmail.com>
2024-07-22 10:15:58 -0700

    removed margins

--- src/components/landing/Introduction.jsx
+++ src/components/landing/Introduction.jsx
@@ -4,28 +4,24 @@ import { TiArrowSortedDown } from "react-icons/ti";
 
 const Introduction = () => {
   return (
-    <div className="pt-8 flex items-stretch justify-between bg-white brightness-95">
-      <Image
-        src={circuitImage}
-        alt="Left Circuit Image"
-        className="w-1/4 mr-5"
-      />
+    <div className="pt-8 flex items-stretch justify-between bg-white">
+      <Image src={circuitImage} alt="Left Circuit Image" className="w-1/4" />
 
-      <div className="w-1/2 flex flex-col">
-        <div className="flex flex-grow flex-col items-center justify-center">
-          <p className="whitespace-nowrap text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl text-black">
+      <div className="w-1/2 flex flex-col items-center justify-center gap-y-4">
+        <div className="flex flex-col items-center justify-center">
+          <p className="whitespace-nowrap text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-black">
             AI @ UCR
           </p>
-          <p className="whitespace-nowrap text-xs sm:text-sm md:text-lg lg:text-xl xl:text-2xl text-black">
+          <p className="whitespace-nowrap text-xs sm:text-sm md:text-lg lg:text-xl text-black">
             Empowering the Future of Tech
           </p>
         </div>
 
-        <div className="flex flex-grow items-center justify-center">
-          <button className="relative aspect-square p-2 md:p-3 xl:p-4 2xl:p-5 text-xs md:text-md lg:text-xl xl:text-2xl 2xl:text-4xl text-white bg-ai-blue-500 rounded-full">
+        <div className="flex items-center justify-center">
+          <button className="relative aspect-square p-2 md:p-3 xl:p-4 2xl:p-5 text-xs md:text-md lg:text-xl text-white bg-ai-blue-500 rounded-full">
             Learn <br /> More
             <div className="absolute inset-x-0 bottom-0 flex justify-center">
-              <TiArrowSortedDown className="text-xs md:text-md lg:text-xl xl:text-2xl 2xl:text-4xl text-white" />
+              <TiArrowSortedDown className="text-xs md:text-md lg:text-xl text-white" />
             </div>
           </button>
         </div>
@@ -34,7 +30,7 @@ const Introduction = () => {
       <Image
         src={circuitImage}
         alt="Right Circuit Image"
-        className="w-1/4 ml-5 scale-x-[-1]"
+        className="w-1/4 scale-x-[-1]"
       />
     </div>
   );
