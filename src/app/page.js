a4c14083fe95d3a73dc33e776bed0aa3bc1aff6d
Andrew Ocegueda <andrewocegueda2018@gmail.com>
2024-07-24 12:21:27 -0700

    Changed the landing page.js file to include all the major components

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,15 @@
 import Introduction from "@/components/landing/Introduction";
+import Information from "@/components/landing/Information";
+import OfferingCards from "@/components/landing/OfferingCards";
 
 const Home = () => {
-  return <Introduction />;
+  return (
+    <>
+      <Introduction />;
+      <Information />;
+      <OfferingCards />;
+    </>
+  );
 };
 
 export default Home;
