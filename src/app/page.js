11bd326bf5c724fad03ee03dec3c43fc3c2db3e7
Rishab Pillai <rishabhpillai.me@gmail.com>
2024-07-28 19:13:04 -0700

    Added responsiveness

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,13 @@
 import OfferingCards from "@/components/landing/OfferingCards";
+import Header from "@/components/Header";
 
 const Home = () => {
-  return <OfferingCards />;
+  return (
+    <div>
+      <Header text="MEET THE BOARD"></Header>
+      <OfferingCards></OfferingCards>
+    </div>
+  );
 };
 
 export default Home;
