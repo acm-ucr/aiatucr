78b425c808edac499c48742823dca281ea4b3aeb
Andrew Ocegueda <andrewocegueda2018@gmail.com>
2024-07-31 15:24:15 -0700

    changed styling in OfferingCard and OfferingCards components so cards can have width and is responsive.

--- src/components/landing/OfferingCard.jsx
+++ src/components/landing/OfferingCard.jsx
@@ -1,6 +1,6 @@
 const OfferingCard = ({ title, description, icon }) => {
   return (
-    <div className="h-full rounded-3xl p-8 bg-ai-blue-200 flex flex-col justify-between">
+    <div className="m-8 h-fit w-10/12 lg:w-2/5 xl:w-1/5 rounded-3xl p-8 bg-ai-blue-200 flex flex-col justify-between">
       <h3 className="text-white text-2xl text-center font-semibold">{title}</h3>
       <div className="flex justify-center items-center">{icon}</div>
       <p className="text-center text-ai-gray">{description}</p>
--- src/components/landing/OfferingCards.jsx
+++ src/components/landing/OfferingCards.jsx
@@ -3,10 +3,10 @@ import { offerings } from "@/data/Offerings";
 
 const OfferingCards = () => {
   return (
-    <section className="p-4">
+    <section className="h-fit p-4">
       <p className="text-4xl text-ai-blue-500 text-center m-8">We Offer...</p>
-      <div className="flex justify-center">
-        <div className="inline-grid grid-cols-1 md:grid-cols-3 gap-8 w-auto place-items-center">
+      <div className="flex justify-center h-full">
+        <div className="w-full flex flex-col lg:flex-row items-center flex-wrap justify-around space-y-4 lg:space-y-0">
           {offerings.map((offering, index) => (
             <OfferingCard
               key={index}
