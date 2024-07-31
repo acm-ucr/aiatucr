8061112a0e3bc09d72e99ce594a1a573170aff1f
Andrew Ocegueda <andrewocegueda2018@gmail.com>
2024-07-31 15:44:25 -0700

    revert back to grid layout for 1024 px width screens or higher

--- src/components/landing/OfferingCards.jsx
+++ src/components/landing/OfferingCards.jsx
@@ -5,8 +5,8 @@ const OfferingCards = () => {
   return (
     <section className="h-fit p-4">
       <p className="text-4xl text-ai-blue-500 text-center m-8">We Offer...</p>
-      <div className="flex justify-center h-full">
-        <div className="w-full flex flex-col lg:flex-row items-center flex-wrap justify-around space-y-4 lg:space-y-0">
+      <div className="flex justify-center">
+        <div className="w-full flex flex-col p-4 items-start space-y-4 lg:space-y-0 lg:grid lg:grid-cols-2 xl:grid-cols-3 gap-8 justify-center">
           {offerings.map((offering, index) => (
             <OfferingCard
               key={index}
