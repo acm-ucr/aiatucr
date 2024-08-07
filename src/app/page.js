ee83f46ff1435437a1645af7942bb1eb2f8a47e1
Andrew Khadder <andrewkh15@gmail.com>
2024-08-06 22:43:23 -0700

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,13 @@
 import Introduction from "@/components/landing/Introduction";
 import Information from "@/components/landing/Information";
 import OfferingCards from "@/components/landing/OfferingCards";
-import Header from "@/components/Header";
 
 const Home = () => {
   return (
     <>
-      <Introduction />;<Header></Header>
-      <Information />;
-      <OfferingCards />;
+      <Introduction />
+      <Information />
+      <OfferingCards />
     </>
   );
 };
