a935a0e7f3692808bf0a897a64b71d3024fdd411
Zachary Nobunaga <znobunaga11@gmail.com>
2024-08-08 17:42:01 -0700

    shortened headers

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,17 @@ import wireframe from "/public/wireframe.webp";
 const Header = ({ text }) => {
   return (
     <div className="bg-ai-blue-400 flex items-center justify-between">
-      <Image className="" src={wireframe} alt="Left wireframe" />
+      <Image className="w-2/6 h-auto" src={wireframe} alt="Left wireframe" />
 
       <div className="py-10">
         <p className="text-center text-white text-4xl font-semibold">{text}</p>
       </div>
 
-      <Image className="rotate-180" src={wireframe} alt="Right wireframe" />
+      <Image
+        className="rotate-180 w-2/6 h-auto"
+        src={wireframe}
+        alt="Right wireframe"
+      />
     </div>
   );
 };
