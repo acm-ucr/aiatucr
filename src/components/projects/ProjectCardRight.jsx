21ab4ccdd1a04a9a3d27026861eb4fe832746428
Houston Gunter <houston@Houstons-Air.attlocal.net>
2024-08-15 22:48:56 -0700

    moved text right

--- src/components/projects/ProjectCardRight.jsx
+++ src/components/projects/ProjectCardRight.jsx
@@ -5,13 +5,13 @@ const ProjectCardRight = ({ projectName, description }) => {
   return (
     <div className="flex flex-col md:flex-row items-center md:items-start">
       <div className="relative py-12">
-        <Image src={projectCard} alt="" className="w-8/12 h-auto ml-44" />
+        <Image src={projectCard} alt="" className="w-11/12 h-11/12 ml-12" />
         <div className="absolute inset-0 flex items-center justify-center">
           <p className="text-5xl text-ai-blue-500 text-center">{projectName}</p>
         </div>
       </div>
-      <div className="w-96 h-auto mt-4 md:mt-0 md:mr-4 w-full max-w-xs">
-        <p className="text-3xl md:text-3xl text-black py-14 pl-16">
+      <div className="w-1/2 h-auto">
+        <p className="text-3xl md:text-3xl text-black pt-40 px-16">
           {description}
         </p>
       </div>
