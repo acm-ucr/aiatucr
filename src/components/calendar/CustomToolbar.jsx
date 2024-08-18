c3e1fbdbeba603a2d2b1c20e47d3e95c6c4ff743
Andrew Ocegueda <andrewocegueda2018@gmail.com>
2024-08-17 21:47:20 -0700

    Change styles for the mobile, tablet and laptop view to better suit each display width and changed the colors of the arrows and date with ai-black.

--- src/components/calendar/CustomToolbar.jsx
+++ src/components/calendar/CustomToolbar.jsx
@@ -5,33 +5,33 @@ import { FaComputer } from "react-icons/fa6";
 const CustomToolbar = ({ onNavigate, date }) => {
   return (
     <div className="flex flex-col xl:flex-row space-y-2 xl:space-y-0">
-      <div className="flex justify-center items-center w-full py-3 flex-row">
-        <div className="flex items-center justify-center md:p-4 p-1 rounded-full b-gray-200 shadow-md mr-2">
+      <div className="flex justify-center items-center py-3 flex-row">
+        <div className="flex items-center justify-center md:p-4 p-1 rounded-full shadow-lg mr-2">
           <AiOutlineArrowLeft
             onClick={() => {
               onNavigate("PREV");
             }}
-            className="hover:cursor-pointer text-4xl text-gray-500 md:max-w-12 max-w-4"
+            className="hover:cursor-pointer text-4xl xl:text-xl text-ai-black md:max-w-12 max-w-4"
           />
         </div>
-        <p className="m-0 bg-clip-text bg-saf-brown md:text-4xl text-lg px-3">
+        <p className="m-0 bg-clip-text bg-saf-brown text-4xl xl:text-xl px-3 text-ai-black text-nowrap">
           {date.toLocaleString("default", { month: "short" })}{" "}
           {date.getFullYear()}
         </p>
-        <div className="flex items-center justify-center md:p-4 p-1 rounded-full b-gray-200 shadow-md mr-2">
+        <div className="flex items-center justify-center md:p-4 p-1 rounded-full shadow-lg mr-2">
           <AiOutlineArrowRight
             onClick={() => onNavigate("NEXT")}
-            className="hover:cursor-pointer text-4xl text-gray-500 md:max-w-12 max-w-4"
+            className="hover:cursor-pointer text-4xl xl:text-xl text-ai-black md:max-w-12 max-w-4"
           />
         </div>
       </div>
-      <div className="flex gap-4 justify-center items-center">
-        <button className="bg-ai-blue-400 p-2 rounded-xl flex justify-center items-center text-sm">
-          <PiBrainBold className="text-2xl mr-2" />
+      <div className="flex flex-col md:flex-row gap-4 xl:w-full justify-center xl:justify-end items-center">
+        <button className="bg-ai-blue-400 px-4 py-2 rounded-full flex justify-center items-center text-base md:text-xs text-nowrap flex-nowrap text-white">
+          <PiBrainBold className="text-lg mr-2" />
           Artificial Intelligence
         </button>
-        <button className="bg-ai-blue-400 p-2 rounded-xl flex justify-center items-center text-sm">
-          <FaComputer className="text-2xl mr-2" />
+        <button className="bg-ai-blue-400 px-4 py-2 rounded-full flex justify-center items-center text-base md:text-xs  text-nowrap flex-nowrap text-white">
+          <FaComputer className="text-lg mr-2" />
           Machine Learning
         </button>
       </div>
