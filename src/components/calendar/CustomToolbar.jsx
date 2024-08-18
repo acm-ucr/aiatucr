527d771ccd8ffdaab76d975f4a6da582089f350d
Andrew Ocegueda <andrewocegueda2018@gmail.com>
2024-08-18 13:18:38 -0700

    Changed style for arrow button so that the circle is not visible. Also got rid of some styling from the template that are not needed no more.

--- src/components/calendar/CustomToolbar.jsx
+++ src/components/calendar/CustomToolbar.jsx
@@ -6,22 +6,22 @@ const CustomToolbar = ({ onNavigate, date }) => {
   return (
     <div className="flex flex-col xl:flex-row space-y-2 xl:space-y-0">
       <div className="flex justify-center items-center py-3 flex-row">
-        <div className="flex items-center justify-center md:p-4 p-1 rounded-full shadow-lg mr-2">
+        <div className="flex items-center justify-center p-1 rounded-full">
           <AiOutlineArrowLeft
             onClick={() => {
               onNavigate("PREV");
             }}
-            className="hover:cursor-pointer text-4xl xl:text-xl text-ai-black md:max-w-12 max-w-4"
+            className="hover:cursor-pointer text-4xl xl:text-xl text-ai-black"
           />
         </div>
-        <p className="m-0 bg-clip-text bg-saf-brown text-4xl xl:text-xl px-3 text-ai-black text-nowrap">
+        <p className="m-0 text-4xl xl:text-xl px-3 text-ai-black text-nowrap">
           {date.toLocaleString("default", { month: "short" })}{" "}
           {date.getFullYear()}
         </p>
-        <div className="flex items-center justify-center md:p-4 p-1 rounded-full shadow-lg mr-2">
+        <div className="flex items-center justify-center p-1 rounded-full">
           <AiOutlineArrowRight
             onClick={() => onNavigate("NEXT")}
-            className="hover:cursor-pointer text-4xl xl:text-xl text-ai-black md:max-w-12 max-w-4"
+            className="hover:cursor-pointer text-4xl xl:text-xl text-ai-black"
           />
         </div>
       </div>
