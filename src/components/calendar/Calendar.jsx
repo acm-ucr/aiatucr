502a0b135a481fde369c8e3481f9d50b8319d827
khushi-04 <khushi.chaudhari@gmail.com>
2024-08-19 21:07:36 -0700

    added earlier code back to calendar.jsx

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -28,6 +28,18 @@ const CalendarEvent = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
+        // incorrect api information
+        // const response = await fetch(
+        //   `https://www.googleapis.com/calendar/v3/calendars/${
+        //     process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_EMAIL
+        //   }/events?key=${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_API_KEY}
+        //   &singleEvents=true&orderBy=startTime&timeMin=${new Date(
+        //     new Date().getTime() - 60 * 60 * 24 * 7 * 10 * 1000
+        //   ).toISOString()}&timeMax=${new Date(
+        //     new Date().getTime() + 60 * 60 * 24 * 7 * 10 * 1000
+        //   ).toISOString()}`
+        // );
+
         const mockEvents = [
           {
             title: "Test Event 1",
@@ -46,6 +58,25 @@ const CalendarEvent = () => {
         ];
 
         setEvents(mockEvents);
+
+        // const offset = new Date().getTimezoneOffset() * 60000;
+        // const data = await response.json();
+
+        // if (data.items) {
+        //   const items = data.items.map((item) => {
+        //     item.allDay = !item.start.dateTime;
+        //     (item.start = item.start.dateTime
+        //       ? new Date(item.start.dateTime)
+        //       : new Date(new Date(item.start.date).getTime() + offset)),
+        //       (item.end = new Date(
+        //         item.end.dateTime || new Date(item.end.date).getTime() + offset
+        //       )),
+        //       (item.hidden = false);
+
+        //     return item;
+        //   });
+        //   setEvents(items);
+        // }
       } catch (error) {
         console.error("Error fetching data: ", error);
       }
