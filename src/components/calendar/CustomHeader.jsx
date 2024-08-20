9cc433390e24f280a6e269bcbee40fb50ae2bc2b
jisubang1001 <bangjisu1001@gmail.com>
2024-08-20 15:34:06 -0400

    Style_Calendar_Weekdays

--- src/components/calendar/CustomHeader.jsx
+++ src/components/calendar/CustomHeader.jsx
@@ -1,8 +1,18 @@
 import "./index.css";
 
 const Header = ({ label }) => {
+  let roundedClass = "";
+
+  if (label === "Sun") {
+    roundedClass = "rounded-tl-lg";
+  } else if (label === "Sat") {
+    roundedClass = "rounded-tr-lg";
+  }
+
   return (
-    <div className="pt-2 h-12 font-medium md:text-2xl text-xs border-t-2 border-gray-500">
+    <div
+      className={`pt-2 pl-1 h-12 bg-ai-gray font-medium md:text-xl text-white text-left text-xs border-black border-t-2 border-b-2 border-l-2 border-r ${roundedClass}`}
+    >
       {label}
     </div>
   );
