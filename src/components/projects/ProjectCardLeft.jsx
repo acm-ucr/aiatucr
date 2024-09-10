b70d0033fbe0a56d22f6459d09b209f03096e89e
Andrew Khadder <andrewkh15@gmail.com>
2024-09-09 23:25:05 -0700

    update projects

--- src/components/projects/ProjectCardLeft.jsx
+++ src/components/projects/ProjectCardLeft.jsx
@@ -1,25 +1,36 @@
 import projectCard from "@/public/images/projectCard.webp";
 import Image from "next/image";
 
-const ProjectCardLeft = ({ projectName, description }) => {
+const ProjectCardLeft = ({
+  projectName,
+  description,
+  leftImage,
+  rightImage,
+}) => {
   return (
-    <div className="flex flex-col-reverse md:flex-row items-center md:items-start">
-      <div className="md:w-1/2 h-auto">
-        <p className="text-2xl md:text-3xl text-black pl-7 pb-12 md:pt-40 md:px-16 font-hubbali">
-          {description}
-        </p>
-      </div>
-      <div className="relative py-12">
-        <Image
-          src={projectCard}
-          alt=""
-          className="w-11/12 h-11/12 ml-5 md:ml-12"
-        />
-        <div className="absolute inset-0 flex items-center justify-center">
-          <p className="text-2xl md:text-5xl text-ai-blue-500 text-center font-montserrat">
-            {projectName}
+    <div>
+      <div className="flex flex-col-reverse lg:flex-row items-center lg:items-start">
+        <div className="md:w-5/6 lg:w-1/2 h-auto">
+          <p className="text-2xl md:text-3xl text-black pl-7 pb-12 lg:pt-40 lg:px-16 font-hubbali">
+            {description}
           </p>
         </div>
+        <div className="relative py-12">
+          <Image
+            src={projectCard}
+            alt=""
+            className="w-11/12 h-11/12 ml-5 md:ml-12"
+          />
+          <div className="absolute inset-0 flex items-center justify-center">
+            <p className="text-2xl md:text-5xl text-ai-blue-500 text-center font-montserrat">
+              {projectName}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col w-full justify-evenly md:flex-row p-3 gap-y-4">
+        <Image src={leftImage} alt="Image 1" className="md:w-2/5 rounded-xl" />
+        <Image src={rightImage} alt="Image 2" className="md:w-2/5 rounded-xl" />
       </div>
     </div>
   );
